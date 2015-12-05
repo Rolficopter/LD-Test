38eb19f10140907f1c4e3a4f3bfe1facba39e3fa
Jan-Henrik Bruhn <jh.bruhn@me.com>
2015-12-06 00:14:23 +0100

    More Changes for walking

--- Assets/Resources/Scripts/WalkingScript.cs
+++ Assets/Resources/Scripts/WalkingScript.cs
@@ -3,10 +3,18 @@ using System.Collections;
 
 public class WalkingScript : StateMachineBehaviour {
 
+    Transform leftPivot;
+    Transform leftLeg;
+    Transform rightPivot;
+    Transform rightLeg;
+
 	 // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-	//override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-	//
-	//}
+	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+        leftPivot = animator.transform.Find("leg_left");
+        leftLeg = leftPivot.Find("left");
+        rightPivot = animator.transform.Find("leg_right");
+        rightLeg = rightPivot.Find("right");
+    }
 
 	// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
 	//override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
@@ -20,12 +28,11 @@ public class WalkingScript : StateMachineBehaviour {
 
 	// OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
 	override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-        Transform leftPivot = animator.transform.Find("leg_left");
-        Transform leftLeg = leftPivot.Find("left");
-        Transform rightPivot = animator.transform.Find("leg_right");
-        Transform rightLeg = rightPivot.Find("right");
 
-        leftLeg.RotateAround(leftPivot.position, leftLeg.forward, 2);
+
+        leftLeg.Rotate(leftLeg.forward, 20 * Time.deltaTime);
+        rightLeg.Rotate(leftLeg.forward, -20 * Time.deltaTime);
+
 	}
 
 	// OnStateIK is called right after Animator.OnAnimatorIK(). Code that sets up animation IK (inverse kinematics) should be implemented here.
