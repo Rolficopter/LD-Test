ed8f4f0cee06c6ae0b646bace430e3c65eae2d21
Sören Gade <gade.soeren@googlemail.com>
2015-12-05 21:47:46 +0100

    Added map and random spawns.
    Wrong scales still.

--- Assets/Listeners.cs
+++ Assets/Listeners.cs
@@ -16,11 +16,27 @@ public class Listeners : MonoBehaviour {
     {
         GameObject newPlayer = PhotonNetwork.Instantiate("CoolerPlayer", Vector3.zero, Quaternion.identity, 0);
         newPlayer.GetComponent<Platformer2DUserControl>().enabled = true;
+        newPlayer.transform.position = this.GetRandomSpawn().position;
 
         // follow new player
         Camera mainCamera = GameObject.Find("Main Camera").GetComponent<Camera>();
         Camera2DFollow cameraFollow = mainCamera.GetComponent<Camera2DFollow>();
         cameraFollow.target = newPlayer.transform;
         cameraFollow.enabled = true;
+    }
+
+    private Transform GetRandomSpawn()
+    {
+        Transform[] spawns = this.GetSpawns();
+
+        int index = Random.Range(0, spawns.Length);
+        return spawns[index];
+    }
+    private Transform[] GetSpawns()
+    {
+        GameObject spawnPointsParent = GameObject.Find("Map/SpawnPoints");
+        Transform[] spawnPoints = spawnPointsParent.GetComponentsInChildren<Transform>();
+
+        return spawnPoints;
     }
 }
