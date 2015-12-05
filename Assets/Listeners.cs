2ecb4be691327cdc8f4ab95b16d26acf24a8ee6c
Jan-Henrik Bruhn <jh.bruhn@me.com>
2015-12-05 20:03:40 +0100

    Added Highly Experimental Network things

---
+++ Assets/Listeners.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Listeners : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnJoinedRoom()
+    {
+        Debug.Log("PETER");
+        PhotonNetwork.Instantiate("CoolerPlayer", Vector3.zero, Quaternion.identity, 0).GetComponent<Platformer2D;
+    }
+}
