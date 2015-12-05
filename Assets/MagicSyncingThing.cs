258b4e9aa0978b695254dee01766e65a483e80e4
Jan-Henrik Bruhn <jh.bruhn@me.com>
2015-12-05 20:16:07 +0100

    Added more amazing things

---
+++ Assets/MagicSyncingThing.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MagicSyncingThing : MonoBehaviour {
+
+    private Vector3 correctPos;
+    private Quaternion correctRotate;
+
+    void Update()
+    {
+        if (!this.GetComponent<PhotonView>().isMine)
+        {
+            transform.position = Vector3.Lerp(transform.position, this.correctPos, Time.deltaTime * 10);
+            transform.rotation = Quaternion.Lerp(transform.rotation, this.correctRotate, Time.deltaTime * 10);
+        }
+    }
+
+	// Use this for initialization
+    void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.isWriting)
+        {
+            stream.SendNext(transform.position);
+            stream.SendNext(transform.rotation);
+        }
+        else
+        {
+            this.correctPos = (Vector3)stream.ReceiveNext();
+            this.correctRotate = (Quaternion)stream.ReceiveNext();
+        }
+    }
+}
